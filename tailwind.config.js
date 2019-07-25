dab9dbf87e4a172b88ee027af74adb7a337470b4
paramagicdev <konnor5456@gmail.com>
2019-07-25 04:13:33 -0400

    Setup purgecss, changed dist/ to src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,14 @@
+// https://tailwindcss.com/docs/controlling-file-size#setting-up-purgecss
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './dist/**/*.html'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   theme: {
     extend: {},
@@ -7,5 +18,11 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+    ? [purgecss]
+    : []
+  ]
 }

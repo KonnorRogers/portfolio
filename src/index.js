e0a4c35f8ae7d7fbd205b0ecaa0b136825cda9ba
paramagicdev <konnor5456@gmail.com>
2019-07-26 23:47:13 -0400

    adding postcss-color-mod-function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './styles.css';
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   element.innerHTML = 'Hello webpack';
-//   element.classList.add('hello');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());

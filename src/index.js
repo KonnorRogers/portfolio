0e1159617845ca168e42c0ec90e5f7c2a1239bc2
paramagicdev <konnor5456@gmail.com>
2019-07-28 15:42:51 -0400

    Attempting to get background image working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// Allows you to import a whole directory
+function importAll(r) {
+  r.keys().forEach(r);
+};
+// import all images
+importAll(require.context('./img/', true, /\.jpg$/));
+
 import './styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
@@ -15,23 +18,60 @@ module.exports = {
         exclude: /node_modules/,
         use: [ 
           {
+            // Extracts the css, supports hot module reloading via hmr
             loader: MiniCssExtractPlugin.loader,
             options: {
               hmr: process.env.NODE_ENV === 'development',
             },
           },
+          // loads css normally
           { loader: 'css-loader', options: { importLoaders: 1 } },
+          // loads css via postcss
           'postcss-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+          loader: 'file-loader',
+            options: { outputPath: 'images' }
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
         ],
       },
     ],
   },
   plugins: [
+    // cleans out the dist/ directory
+    new CleanWebpackPlugin(),
+
+    // generates a new HTML file @ dist/index.html
+    new HtmlWebpackPlugin({
+      'meta': { 
+        'viewport': 'width=width-device, initial-scale=1'
+      },
+      title: "Konnor's portfolio",
+      template: './src/index.html'
+    }),
+    // Extracts the css
     new MiniCssExtractPlugin({
       filename: "styles.css",
       chunkFilename: "styles.css"
     }),
   ],
+  
+  // Used for webpack-dev-server
   devServer: {
     contentBase: path.resolve(__dirname, "dist"),
     watchContentBase: true

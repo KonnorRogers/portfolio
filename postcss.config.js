b69514534e04bf09a6fea31701b1dd4909d2ee4f
paramagicdev <konnor5456@gmail.com>
2019-07-29 03:59:01 -0400

    Added annotations to postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,22 @@
+// Allows @import "" statements
 const postcssImport = require('postcss-import')
+
+// pulls in tailwind
 const tailwindcss = require('tailwindcss')('./tailwind.config.js')
+
+// allows color-mod() to be called in place of rgba / hsla
 const colorMod = require('postcss-color-mod-function')
+
+// Adds ::before & ::after and other useful prefixes
 const autoprefixer = require('autoprefixer')
+
+// Allows nesting
 const nested = require('postcss-nested')
+
+// Removes unused css
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html'],
+  // Whitelist any patterns css that is dynamically loaded
   whitelistPatternsChildren: [/rotate/],
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],

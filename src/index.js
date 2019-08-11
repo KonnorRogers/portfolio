c42643053148090393a27a9705d4fdb523c41498
paramagicdev <konnor5456@gmail.com>
2019-08-11 18:35:39 -0400

    finished final touches

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function importAll(r) {
   r.keys().forEach(r);
 };
 // import all images
-importAll(require.context('./img/', true, /\.jpg$/));
+importAll(require.context('./img/', true, /\.(png|jpe?g|gif)$/));
 
 import './styles.css';
 import './js/toggleMenu.js';

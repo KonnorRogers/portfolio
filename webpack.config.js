54b69ffefce1480ce439ffc1fb8da9f83ddff988
paramagicdev <konnor5456@gmail.com>
2019-10-11 01:01:32 -0400

    adding bundles, trying to get tailwind working

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.jsx?$/,
-        use: ['babel-loader', 'astroturf/loader'],
-      },
-    ],
-  },
-
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -28,4 +16,24 @@ module.exports = {
     port: 8080,
     open: true,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader', 'astroturf/loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };

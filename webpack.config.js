ce90436df968f5b405e91d48489df912697dba04
paramagicdev <konnor5456@gmail.com>
2019-10-11 01:18:13 -0400

    config stuff

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,25 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'postcss-loader'],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        ],
       },
       {
         test: /\.jsx?$/,

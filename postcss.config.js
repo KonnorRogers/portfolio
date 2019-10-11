34aa0c057a5c38ba932decf76259c682e00dfcf3
paramagicdev <konnor5456@gmail.com>
2019-10-11 00:11:59 -0400

    version 1 of portfolio

---
+++ postcss.config.js
@@ -0,0 +1,36 @@
+// Allows @import "" statements
+const postcssImport = require('postcss-import')
+
+// pulls in tailwind
+const tailwindcss = require('tailwindcss')('./tailwind.config.js')
+
+// allows color-mod() to be called in place of rgba / hsla
+const colorMod = require('postcss-color-mod-function')
+
+// Adds ::before & ::after and other useful prefixes
+const autoprefixer = require('autoprefixer')
+
+// Allows nesting
+const nested = require('postcss-nested')
+
+// Removes unused css
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.html'],
+  // Whitelist any patterns css that is dynamically loaded
+  whitelistPatternsChildren: [/rotate/, /trans/, /slide/, /nav/],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
+
+module.exports = {
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    autoprefixer,
+    nested,
+    colorMod,
+
+    // if its production, purge it, if its not, leave it alone
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // 'primary': '#444',
+        'primary': '#5B618A',
+        'secondary': '#4fd1c5',
+        // 'secondary': '#eece1a',
+        'primary-text': '#fff'
+      },
+      fontSize: {
+        '8xl': '6rem'
+      },
+      zIndex: {
+        '2': 2,
+        '-1': '-1'
+      },
+      margin: {
+        '20vh': '20vh'
+      },
+      inset: {
+        '2': '2rem'
+      },
+      height: {
+        'tiny': '3px',
+        '50px': '50px',
+        '75vh': '75vh',
+        '25vh': '25vh',
+        '72': '18rem',
+        '3/4': '75%',
+        '1/2': '50%',
+        '1/3': '33%',
+        '44': '11rem'
+      },
+      opacity: {
+        '90': '.9'
+      },
+    },
+  },
+  variants: {
+    opacity: ['responsive', 'hover'],
+    padding: ['responsive', 'hover']
+  },
+  plugins: []
+}

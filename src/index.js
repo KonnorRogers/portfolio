6735284f4e440696040aa24aa21a1246a6fc98f5
paramagicdev <konnor5456@gmail.com>
2019-10-12 15:22:47 -0400

    comment out index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // Allows you to import a whole directory
-function importAll(r) {
-  r.keys().forEach(r);
-};
+// function importAll(r) {
+// r.keys().forEach(r);
+// };
 // import all images
-importAll(require.context('./img/', true, /\.(png|jpe?g|gif)$/));
+// importAll(require.context('./img/', true, /\.(png|jpe?g|gif)$/));
 
-import './styles.css';
-import './js/toggleMenu.js';
-
+// import './styles.css';
+// import './js/toggleMenu.js';

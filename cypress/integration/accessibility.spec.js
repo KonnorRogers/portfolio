017bb65ba48382b6fee30d43ea19448465d4b47b
ParamagicDev <konnor5456@gmail.com>
2020-06-30 18:35:08 -0400

    tests

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -5,18 +5,23 @@ describe("Accessibility tests", () => {
     cy.visit("/").get("main").injectAxe();
   });
 
-  it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y();
-    cy.viewport("iphone-3").checkA11y();
+  it("checks nav-button for accessibility", () => {
+    cy.viewport("iphone-3");
+    cy.get("[data-controller='nav-button']").click();
+    cy.get("[data-target='nav-button.content']").injectAxe().checkA11y();
   });
 
   it("navigates to each page and tests accessibility", () => {
+    cy.checkA11y();
+    cy.viewport("iphone-3").checkA11y();
+
     cy.get("[data-controller='nav-bar']")
       .find("a")
       .each((child) => {
         cy.visit(child.attr("href"));
         cy.get("main").injectAxe();
         cy.checkA11y();
+        cy.viewport("iphone-3").checkA11y();
       });
   });
 });

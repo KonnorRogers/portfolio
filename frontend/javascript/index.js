ae21af1b4fff1b4da7622b02aee54e09c2087faf
paramagicdev <konnor5456@gmail.com>
2020-07-04 17:51:32 -0400

    something went wrong..

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,6 @@
 import "../styles/index.css";
-import { Application } from "stimulus";
-import Turbolinks from "turbolinks";
+// import { Application } from "stimulus";
+// import Turbolinks from "turbolinks";
 
 console.info("Bridgetown is loaded!");
 // const application = Application.start();

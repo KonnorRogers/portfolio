2a199ab8de8bec9fc51587bf2898912e97b2c058
paramagicdev <konnor5456@gmail.com>
2020-07-08 19:47:05 -0400

    lots of changes

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,11 @@
-import * as Stimulus from "stimulus"
+// import "../styles/index.css"
+import { Application } from "stimulus"
 import Turbolinks from 'turbolinks';
 
 import NavBarController from "./controllers/nav_bar_controller.js";
 import NavButtonController from "./controllers/nav_button_controller.js";
 
-const application = Stimulus.Application.start();
+const application = Application.start();
 application.register("nav-bar", NavBarController);
 application.register("nav-button", NavButtonController);
 Turbolinks.start();

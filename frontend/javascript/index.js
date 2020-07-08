61f05df11e06dd4d16a9de482330e6b578b7e487
paramagicdev <konnor5456@gmail.com>
2020-07-08 16:06:13 -0400

    add stimulus & turbolinks

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,8 @@
 import "../styles/index.css";
-import { Application } from "https://cdn.pika.dev/stimulus@^1.1.1";
-import Turbolinks from "https://cdn.pika.dev/turbolinks@^5.2.0";
+import { Application } from "stimulus"
+import Turbolinks from "turbolinks"
+// import * as Application from "https://cdn.pika.dev/stimulus@^1.1.1";
+// import Turbolinks from "https://cdn.pika.dev/turbolinks@^5.2.0";
 
 import NavBarController from "./controllers/nav_bar_controller.js";
 import NavButtonController from "./controllers/nav_button_controller.js";

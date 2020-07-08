76f455826445a4d4b25e630436a939846186a32b
paramagicdev <konnor5456@gmail.com>
2020-07-08 15:45:19 -0400

    refactor: making things more snowpack friendly

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,9 +1,11 @@
-import "../styles/index.css"
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-import Turbolinks from "turbolinks";
+import "../styles/index.css";
+import { Application } from "https://cdn.pika.dev/stimulus@^1.1.1";
+import Turbolinks from "https://cdn.pika.dev/turbolinks@^5.2.0";
+
+import NavBarController from "./controllers/nav_bar_controller.js";
+import NavButtonController from "./controllers/nav_button_controller.js";
 
 const application = Application.start();
-const context = require.context("./controllers", true, /.js$/);
-application.load(definitionsFromContext(context));
+application.register("nav-bar", NavBarController);
+application.register("nav-button", NavButtonController);
 Turbolinks.start();

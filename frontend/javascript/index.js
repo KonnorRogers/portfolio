6c428b757d17d28fd69be1804555c1ee6b31e9e0
paramagicdev <konnor5456@gmail.com>
2020-07-09 02:11:50 -0400

    fix: no more parcelRequire is not defined

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,6 +4,9 @@ import Turbolinks from 'turbolinks';
 import NavBarController from "./controllers/nav_bar_controller.js";
 import NavButtonController from "./controllers/nav_button_controller.js";
 
+// fix a Parcel issue
+window.parcelRequire = null
+
 const application = Application.start();
 application.register("nav-bar", NavBarController);
 application.register("nav-button", NavButtonController);
